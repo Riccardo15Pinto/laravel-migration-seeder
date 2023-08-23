27521a8c1427c4df236854aa6136e38a0f2f903d
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-23 17:34:54 +0200

    use like general layouts

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ env('APP_NAME') }} | @yield('title')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            display: none;
+        }
+    </style>
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body class="bg-primary-subtle">
+
+    @include('includes.header')
+
+    <main>
+
+        <div class="container my-4">
+
+            @yield('content')
+
+        </div>
+
+    </main>
+
+</body>
+
+</html>
