5b82214455c70574812529f13311c89081bf91ed
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-23 16:15:55 +0200

    create template , and print element from db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,14 +18,51 @@
 <body>
 
     <main class="bg-light">
-        <ul>
 
-            @foreach ($trains as $train)
-                <li>
-                    {{ $train->company }}
-                </li>
-            @endforeach
-        </ul>
+        <div class="container">
+
+            <table class="table table-dark table-striped">
+
+                <thead>
+
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Compagnia</th>
+                        <th scope="col">Partenza</th>
+                        <th scope="col">Destinazione</th>
+                        <th scope="col">Orario di partenza</th>
+                        <th scope="col">Orario di arrivo</th>
+                        <th scope="col">PNR</th>
+                        <th scope="col">Carrozza</th>
+                        <th scope="col">In Orario</th>
+                        <th scope="col">Cancellato</th>
+                    </tr>
+
+                </thead>
+
+                <tbody>
+
+                    @foreach ($trains as $train)
+                        <tr>
+                            <th scope="row">-</th>
+                            <td>{{ $train->company }}</td>
+                            <td>{{ $train->departure }}</td>
+                            <td>{{ $train->destination }}</td>
+                            <td>{{ $train->departure_time }}</td>
+                            <td>{{ $train->arrival_time }}</td>
+                            <td>{{ $train->pnr }}</td>
+                            <td>{{ $train->train_carriage }}</td>
+                            <td>{{ $train->isinTime() }}</td>
+                            <td class="text-danger">{{ $train->isDeleted() }}</td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+
+            </table>
+
+        </div>
+
     </main>
 
 </body>
