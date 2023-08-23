00f361dc0d4fc88ba1bcb8ec44b972334ad10cfa
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-23 17:35:31 +0200

    set ticket page

---
+++ resources/views/ticket.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('title', 'Ticket')
+
+@section('content')
+    <div class="container text-center">
+
+        <h1 class="my-5">Abbiamo finito i treni</h1>
+    </div>
+@endsection
