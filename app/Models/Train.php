9c79cea30bc5b0b88ea029f8190639f77c378754
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-23 16:15:31 +0200

    add 2 pubblic function

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Train extends Model
 {
     use HasFactory;
+
+    public function isInTime()
+    {
+
+        $timing = !$this->in_time ? 'In Ritardo' : 'In Orario';
+
+        return $timing;
+    }
+
+    public function isDeleted()
+    {
+
+        $run = !$this->deleted ? 'Cancellato' : '';
+
+        return $run;
+    }
 }
